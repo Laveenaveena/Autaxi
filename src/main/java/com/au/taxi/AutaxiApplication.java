e18601af0cee0984c8c83e2f0dc2f503db636d82
Administrator <Administrator@MR-N4FBMPGLVUDQ>
2016-10-21 21:55:17 +0700

    initiate spring boot application

---
+++ src/main/java/com/au/taxi/AutaxiApplication.java
@@ -0,0 +1,12 @@
+package com.au.taxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutaxiApplication.class, args);
+	}
+}
